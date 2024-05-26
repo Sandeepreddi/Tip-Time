d985e4c5ff3defe0e062d33acbdb5026fa5b2f7b
Malgireddy Sandeep Reddy <sandeepreddi23@gmail.com>
2024-05-26 19:25:47 +0530

    test written

--- app/src/test/java/com/example/tiptime/ExampleUnitTest.kt
+++ app/src/test/java/com/example/tiptime/ExampleUnitTest.kt
@@ -3,6 +3,7 @@ package com.example.tiptime
 import org.junit.Test
 
 import org.junit.Assert.*
+import java.text.NumberFormat
 
 /**
  * Example local unit test, which will execute on the development machine (host).
@@ -12,6 +13,10 @@ import org.junit.Assert.*
 class ExampleUnitTest {
     @Test
     fun addition_isCorrect() {
-        assertEquals(4, 2 + 2)
+        val amount = 10.00
+        val tipPercent = 20.00
+        val expectedTip = NumberFormat.getCurrencyInstance().format(2)
+        val actualTip = calculateTip(amount = amount, tipPercent = tipPercent, false)
+        assertEquals(expectedTip, actualTip)
     }
 }
